544f0d711c4523a942b2129289e2e3e51929bb52
kt <kthobbs@mail.ubc.ca>
2025-07-03 17:52:29 -0400

    Added spatial join to state boundaries.

--- 3_random_sample.R
+++ 3_random_sample.R
@@ -13,25 +13,27 @@ n <- 25 # sample size
 regions <- st_read(sprintf("input/settlement_areas/%s/%s_preselected.geojson",country, country))
 
 # Filter minimum cluster population
-# - we need at least 500 people in a town for it to be suitable
-# - this is a conservative assumption because WRA make up around 25% of populations or less. 
+# - we need at least 400 people in a town for it to be suitable
+# - this is a conservative assumption because WRA comprise around 25% of populations or less. 
 # I round down to 20% to be conservative. I expect 25% will be unavailable on day of interview.
-# npop = (n households x guess of average HH size) / probability of WRA (20% x 25%) = 100/10% = 500
+# npop = (n WRA to interview) / probability of interviewing WRA (20% x 25%) = 20/10% = 400
 regions_filt <- regions %>%
   filter(
-    X_sum >= 500
+    X_sum >= 400
   )
 
 # Clean result and add selection probabilities based off population proportion
 regions_sf <- regions_filt %>%
   mutate(
     population = X_sum,
-    proportion = population/sum(population)
+    proportion = population/sum(population),
+    state = shapeName
   ) %>%
   select(
     geom_id,
-    ADM3_NAME,
-    NAME,
+    place,
+    name,
+    state,
     population,
     proportion
   ) 
@@ -50,6 +52,8 @@ idx_sample <- sample(
   replace = T,
   prob = regions_df$proportion
 )
+
+sample_sf <- regions_sf[idx_sample, ]
 
 # Summary of sample population
 summary(sample_sf$population)
